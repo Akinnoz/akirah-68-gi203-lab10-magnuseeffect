72e18159e2b5ce4ca36f0d139248d80f4790b825
Akinnoz <switchxm@gmail.com>
2026-03-27 09:05:20 +0700

    implement magnus effect for curved soccer free kick

---
+++ lab10/Assets/script/MagnusSoccerKick.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class MagnusSoccerKick : MonoBehaviour
+{
+    public float kickForce = 30f;
+    public float spinForce = 10f;
+    public float magnusStrength = 4f;
+
+    Rigidbody rb;
+    bool kicked = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !kicked)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+
+            rb.AddForce(Vector3.right * kickForce, ForceMode.Impulse);
+
+            rb.AddTorque(Vector3.up * spinForce, ForceMode.Impulse);
+
+            kicked = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (kicked)
+        {
+            Vector3 magnus = Vector3.Cross(rb.angularVelocity, rb.velocity) * magnusStrength;
+            rb.AddForce(magnus);
+        }
+    }
+}
