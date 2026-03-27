79ff857c49169b3b6e5cc5cdbaf677ce40674667
Akinnoz <switchxm@gmail.com>
2026-03-27 07:52:42 +0700

    implement angular velocity and torque rotation scripts

---
+++ lab10/Assets/script/AngularVelocity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class AngularVelocity : MonoBehaviour
+{
+    public float angularSpeed = 5f;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            // หมุนทันที
+            rb.angularVelocity = new Vector3(0, angularSpeed, 0);
+        }
+        else
+        {
+            // Exercise 1.1: ปล่อยปุ่มแล้วหยุดทันที
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
---
+++ lab10/Assets/script/TorqueRotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TorqueRotate : MonoBehaviour
+{
+    public float torquePower = 5f;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            // Exercise 1.2: หมุนแกน Z
+            rb.AddTorque(new Vector3(0, 0, torquePower));
+        }
+    }
+}
